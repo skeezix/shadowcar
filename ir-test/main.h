bc1be84c36d10a9537755a1c24f7b3b8264790df
Jeff <skeezix@skeleton.org>
2014-10-01 00:24:50 -0400

    PReliminary distance sensor fiddling

---
+++ ir-test/main.h
@@ -0,0 +1,17 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+
+// avoid push in main
+int main( void )        __attribute__((OS_main));
+
+#include "avrutil.h"
+
+#define F_CPU   8000000UL /* 20MHz */
+#include <util/delay.h>
+
+#define BAUD 9600 /* for sabertooth motor controller */
+
+#endif
